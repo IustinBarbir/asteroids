47b4ca04c585a72ad9b1590afc01037d108ab7ec
IustinBarbir <iustin_barbir@yahoo.ro>
2025-11-15 14:52:17 +0200

    Update game loop with FPS control and dt

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 from constants import SCREEN_WIDTH , SCREEN_HEIGHT , ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE,  ASTEROID_MAX_RADIUS
 import pygame
+from logger import log_state
 
 def main():
     pygame.init()
 
-    print("Starting Asteroids!")
-    print("Screen width: " + str(SCREEN_WIDTH)) 
-    print("Screen height: " + str(SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while(True):
+        log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+            
+        screen.fill("black")
         pygame.display.flip()
-
-
+        
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
